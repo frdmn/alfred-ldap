e12b91f406a7c70bfc93a78eef1199c4d6aab746
Jonas Friedmann <j@frd.mn>
2014-08-26 13:51:03 +0200

    Initial commit

---
+++ ldap.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Read LDAP configuration/settings
+source ${HOME}/.alfred-ldap.conf
+
+# Store arguments in variable
+QUERYSTRING="${*}"
+
+# Incremential helper
+i=0
+
+# Check for any arguments
+if [[ -z ${QUERYSTRING} ]]; then
+	echo "Error: no argument"
+	exit 1
+fi
+
+# Run LDAP query and store raw response
+LDAPRAW=$(ldapsearch -x -h ${LDAPHOSTNAME} -p $LDAPPORT -D ${LDAPUSERNAME} -w ${LDAPPASSWORD} -b "${LDAPSEARCHBASE}" "(sAMAccountName=*${QUERYSTRING}*)")
+
+# Count entries and responses
+LDAPENTRIES=$(echo "${LDAPRAW}" | grep "numEntries" | sed 's/\# numEntries: //g')
+LDAPRESPONSES=$(echo "${LDAPRAW}" | grep "numResponses" | sed 's/\# numResponses: //g')
+
+# Check for empty entries
+if [[ -z ${LDAPENTRIES} ]]; then
+	echo "Error: No one found for '${QUERYSTRING}'"
+	exit 1
+fi
+
+# Set delimiter to new line
+IFS="
+"
+
+# Add to arrays
+PHONEARRAY=($(echo "${LDAPRAW}" | sed -n 's/^[ \t ]*telephoneNumber:[ \t ]*\(.*\)/\1/p'))
+NAMEARRAY=($(echo "${LDAPRAW}" | sed -n 's/^[ \t ]*displayName:[ \t ]*\(.*\)/\1/p'))
+
+# For each array element, display name and phone number
+for i in "${!PHONEARRAY[@]}"; do
+
+	echo "<item uid=\"\" arg=\""${PHONEARRAY[$i]/#$PHONEPREFIX/}"\"><title>"${NAMEARRAY[$i]}" ("${PHONEARRAY[$i]/#$PHONEPREFIX/}")</title><subtitle></subtitle><icon>icon/user.png</icon></item>"
+done
